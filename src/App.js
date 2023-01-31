6e4bb10fd8038a663b3325163b425030a26dbb93
Steven D. Meacham <2558575+stevemeacham@users.noreply.github.com>
2023-01-31 11:56:23 -1000

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,14 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello from V2</h1>
       </header>
     </div>
   );
